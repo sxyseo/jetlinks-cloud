951085780e0b10cd9fac686aa94f9ae4c450a172
zhouhao <zh.sqy@qq.com>
2019-03-19 18:59:11 +0800

    完善消息收发

---
+++ common-components/service-dependencies/src/main/java/org/jetlinks/cloud/DeviceConfigKey.java
@@ -0,0 +1,37 @@
+package org.jetlinks.cloud;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hswebframework.web.dict.EnumDict;
+import org.jetlinks.protocol.metadata.ValueWrapper;
+import org.jetlinks.registry.api.DeviceOperation;
+
+/**
+ * 设备配置常量
+ *
+ * @author zhouhao
+ * @since 1.0.0
+ */
+@Getter
+@AllArgsConstructor
+public enum DeviceConfigKey implements EnumDict<String> {
+    deviceConnectTopic("device:topic:connect", "设备连接"),
+    deviceDisconnectTopic("device:topic:disconnect", "设备断开连接"),
+    eventTopic("device:topic:event", "设备事件上报"),
+    functionReplyTopic("device:topic:async:function:reply", "功能调用回复"),
+    ;
+
+    private String value;
+    private String text;
+
+    public ValueWrapper getConfigValue(DeviceOperation operation, String suffix) {
+        return operation.getConfig()
+                .get(getValue().concat(suffix))
+                .notPresent(() -> getConfigValue(operation));
+    }
+
+    public ValueWrapper getConfigValue(DeviceOperation operation) {
+        return operation.getConfig()
+                .get(getValue());
+    }
+}
--- device-gateway-service/src/main/java/org/jetlinks/cloud/device/gateway/handler/DeviceMessageReplyHandler.java
+++ device-gateway-service/src/main/java/org/jetlinks/cloud/device/gateway/handler/DeviceMessageReplyHandler.java
@@ -1,9 +1,15 @@
 package org.jetlinks.cloud.device.gateway.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hswebframework.web.NotFoundException;
+import org.jetlinks.cloud.DeviceConfigKey;
 import org.jetlinks.cloud.device.gateway.vertx.DeviceMessageEvent;
+import org.jetlinks.gateway.session.DeviceSession;
 import org.jetlinks.protocol.message.DeviceMessage;
 import org.jetlinks.protocol.message.DeviceMessageReply;
+import org.jetlinks.protocol.message.event.EventMessage;
+import org.jetlinks.protocol.message.function.FunctionInvokeMessageReply;
+import org.jetlinks.protocol.metadata.FunctionMetadata;
 import org.jetlinks.registry.api.DeviceMessageHandler;
 import org.jetlinks.registry.api.DeviceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +17,17 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.List;
+
+import static org.jetlinks.cloud.DeviceConfigKey.*;
+
 /**
  * @author bsetfeng
  * @version 1.0
- * @Date 2019/3/14 9:38 PM
  **/
 @Component
 @Slf4j
 public class DeviceMessageReplyHandler {
-
-    @Autowired
-    private DeviceRegistry deviceRegistry;
 
     @Autowired
     private DeviceMessageHandler deviceMessageHandler;
@@ -33,8 +39,44 @@ public class DeviceMessageReplyHandler {
             log.warn("消息无messageId:{}", deviceMessage.toJson());
             return;
         }
+
+        DeviceSession session = event.getSession();
+        //设备回复的消息
         if (deviceMessage instanceof DeviceMessageReply) {
-            deviceMessageHandler.reply(deviceMessage);
+            if (deviceMessage instanceof FunctionInvokeMessageReply) {
+                FunctionInvokeMessageReply invokeMessage = ((FunctionInvokeMessageReply) deviceMessage);
+
+                // 设备配置了转发到指定的topic
+                functionReplyTopic
+                        .getConfigValue(session.getOperation(), invokeMessage.getFunctionId())
+                        .asList(String.class)
+                        .ifPresent(topics -> sendMessageToTopic(topics, deviceMessage));
+
+                //判断是否为异步操作，如果不异步的，则需要回复
+                boolean async = session.getOperation()
+                        .getMetadata()
+                        .getFunction(invokeMessage.getFunctionId())
+                        .map(FunctionMetadata::isAsync)
+                        .orElse(false);
+                //同步操作则直接返回
+                if (!async) {
+                    deviceMessageHandler.reply(deviceMessage);
+                }
+            } else if (deviceMessage instanceof EventMessage) {
+                EventMessage eventMessage = ((EventMessage) deviceMessage);
+                eventTopic.getConfigValue(session.getOperation(), eventMessage.getEvent())
+                        .asList(String.class)
+                        .ifPresent(topics -> sendMessageToTopic(topics, deviceMessage));
+            } else {
+                //其他消息 直接回复
+                // FIXME: 19-3-19 还有更好的操作？
+                deviceMessageHandler.reply(deviceMessage);
+            }
         }
+    }
+
+    private void sendMessageToTopic(List<String> topics, DeviceMessage message) {
+        log.info("发送消息到topic:{}\n{}", message.toJson());
+
     }
 }
